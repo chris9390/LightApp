2b953e812f7db864def3ffb74f496645bc6a80bf
Young-in Kim <chris9390@naver.com>
2018-03-28 14:33:09 +0900

    Cleaning SW module, Use updateUI()

--- FirstApp3/ViewController.swift
+++ FirstApp3/ViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
-
+    
+    @IBOutlet weak var messageButton: UIButton!
+    
+    
     var lightOn = true
     
     
@@ -27,14 +30,23 @@ class ViewController: UIViewController {
 
     @IBAction func doButtonTap(_ sender: UIButton) {
         print("Button Touched")
+        updateUI()
+        lightOn = !lightOn
+    }
+    
+    //Cleaning
+    func updateUI() {
         if lightOn == true{
             self.messageLabel.text = "White"
-            view.backgroundColor = UIColor.black
+            self.messageLabel.textColor = .black
+            view.backgroundColor = .white
+            self.messageButton.setTitle("Off", for: .normal)
         } else {
             self.messageLabel.text = "Black"
-            view.backgroundColor = UIColor.white
+            self.messageLabel.textColor = .white
+            view.backgroundColor = .black
+            self.messageButton.setTitle("On",for: .normal)
         }
-        lightOn = !lightOn
     }
 
 }
